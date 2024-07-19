96756852edb63a507ed87dd9a647be142c3b8c2a
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-07-19 11:20:35 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PeEU2RpUaHLNF60O7twW34yHBKHl14IpODfDd3GlOHYtYquSqqpT2ZXBKJwIeyeyxiH1CPS3KL60CCjdDg6dbJA00RvKogCvf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
